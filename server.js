ce405e54134cf18eb668f0666d35859c2595ca69
Harish-b-13 <harishbalaji369@gmail.com>
2021-11-25 21:48:05 +0530

    Fixed Server error - Now Home and Editor Page is WORKING

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.get("/:blog", (req, res) => {
     res.sendFile(path.join(initial_path,"blog.html"));
 })
 
-app.use((req, res) => {
-    res.json("404");
-})
+// app.use((req, res) => {
+//     res.json("404");
+// })
 
 // upload link
 app.post('/upload',(req, res) => {
